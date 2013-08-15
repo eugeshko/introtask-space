76feb240ca75e4ad08503c085a27ac81aa27b041
Evgeniya Kostenko <erunda_88@mail.ru>
2013-08-15 22:27:10 +0400

    Planet's functions completed

--- task.js
+++ task.js
@@ -91,19 +91,33 @@ Vessel.prototype.flyTo = function (newPosition) {
  * @param {Number}[] position Местоположение планеты.
  * @param {Number} availableAmountOfCargo Доступное количество груза.
  */
-function Planet(name, position, availableAmountOfCargo) {}
+function Planet(name, position, availableAmountOfCargo) {
+  this.name = name;
+  this.position = position;
+  this.availableAmountOfCargo = availableAmountOfCargo;
+}
 
 /**
  * Выводит текущее состояние планеты: имя, местоположение, количество доступного груза.
  * @name Planet.report
  */
-Planet.prototype.report = function () {}
+Planet.prototype.report = function () {
+  var text = 'Планета "' + this.name + '". Местоположение: ' + this.position + ".";
+    if (this.getAvailableAmountOfCargo() == 0) {
+      text += " Грузов нет.";
+    } else {
+      text += ' Доступно груза: ' + this.getAvailableAmountOfCargo() + 'т.';
+    }
+  document.write(text + "<br>");
+}
 
 /**
  * Возвращает доступное количество груза планеты.
  * @name Vessel.getAvailableAmountOfCargo
  */
-Planet.prototype.getAvailableAmountOfCargo = function () {}
+Planet.prototype.getAvailableAmountOfCargo = function () {
+  return this.availableAmountOfCargo;
+}
 
 /**
  * Загружает на корабль заданное количество груза.
@@ -113,7 +127,22 @@ Planet.prototype.getAvailableAmountOfCargo = function () {}
  * @param {Number} cargoWeight Вес загружаемого груза.
  * @name Vessel.loadCargoTo
  */
-Planet.prototype.loadCargoTo = function (vessel, cargoWeight) {}
+Planet.prototype.loadCargoTo = function (vessel, cargoWeight) {
+  if (matchPosition(vessel, this)) {
+    if (vessel.getFreeSpace() >= cargoWeight) {
+      if (this.getAvailableAmountOfCargo() >= cargoWeight) {
+        vessel.loadCargoTo(cargoWeight);
+        this.availableAmountOfCargo -= cargoWeight;
+      } else {
+        writeError("На планете недостаточно груза!" );
+      }
+    } else {
+      writeError("На корабле недостаточно места!" );
+    }
+  } else {
+    writeError("Корабль должен приземлиться на планету!" );
+  }
+}
 
 /**
  * Выгружает с корабля заданное количество груза.
@@ -123,4 +152,34 @@ Planet.prototype.loadCargoTo = function (vessel, cargoWeight) {}
  * @param {Number} cargoWeight Вес выгружаемого груза.
  * @name Vessel.unloadCargoFrom
  */
-Planet.prototype.unloadCargoFrom = function (vessel, cargoWeight) {}
+Planet.prototype.unloadCargoFrom = function (vessel, cargoWeight) {
+  if (matchPosition(vessel, this)) {
+    if (vessel.getOccupiedSpace() >= cargoWeight) {
+      vessel.loadCargoTo(- cargoWeight);
+      this.availableAmountOfCargo += cargoWeight;
+    } else {
+      writeError("Недостаточно груза на корабле!")
+    }
+  } else {
+    writeError("Корабль должен приземлиться на планету!");
+  }
+}
+var vessel = new Vessel('Яндекс', [0,0], 1000);
+var planetA = new Planet('A', [0,0], 0);
+var planetB = new Planet('B', [100, 100], 5000);
+
+// Проверка текущего состояния
+vessel.report(); // Корабль "Яндекс". Местоположение: 0,0. Занято: 0 из 1000т.
+planetA.report(); // Планета "A". Местоположене: 0,0. Грузов нет.
+planetB.report(); // Планета "B". Местоположене: 100,100. Доступно груза: 5000т.
+
+vessel.flyTo(planetB);
+
+planetB.loadCargoTo(vessel, 1000);
+vessel.report(); // Корабль "Яндекс". Местоположение: 100,100. Занято: 1000 из 1000т.
+
+vessel.flyTo(planetA);
+planetA.unloadCargoFrom(vessel, 500);
+vessel.report(); // Корабль "Яндекс". Местоположение: 0,0. Занято: 500 из 1000т.
+planetA.report(); // Планета "A". Местоположение: 0,0. Доступно груза: 500т.
+planetB.report(); // Планета "B". Местоположение: 100,100. Доступно груза: 4000т.
